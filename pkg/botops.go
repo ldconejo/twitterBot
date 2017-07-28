30cee3e744b06d16d9be50bc82a677f27994cfc3
ldconejo <luisconej@gmail.com>
2017-07-27 22:44:45 -0700

    Test commit with VSCode

--- pkg/botops.go
+++ pkg/botops.go
@@ -2,14 +2,14 @@ package pkg
 
 import (
 	"fmt"
-	"strings"
-	"regexp"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/dghubble/go-twitter/twitter"
 )
 
-// Decodes direct messages to master account
+// /Decodes direct messages to master account
 // Returns a string and an array containing the type of message
 // and any parameters
 func DecodeMasterMessage(masterMessage string) (bool, string, string) {
@@ -17,7 +17,7 @@ func DecodeMasterMessage(masterMessage string) (bool, string, string) {
 	var result bool
 	var commandArray []string
 
-	if validCommand.MatchString(masterMessage){
+	if validCommand.MatchString(masterMessage) {
 		commandArray = validCommand.FindStringSubmatch(masterMessage)
 		command := commandArray[1]
 		commandParameters := commandArray[2]
@@ -35,117 +35,117 @@ func DecodeMasterMessage(masterMessage string) (bool, string, string) {
 
 // Act on master message
 // Uses the output from decodeMasterMessage and takes action on it
-func ActOnMasterMessage (client *twitter.Client, master string, servant string, retweetCandidate *twitter.Tweet, result bool, command string, commandParameters string )  {
-	if  result == true {
+func ActOnMasterMessage(client *twitter.Client, master string, servant string, retweetCandidate *twitter.Tweet, result bool, command string, commandParameters string) {
+	if result == true {
 		switch command {
-			// Command is to tweet something
-			case "TWT":
-				// Tweets the command parameters and sends confirmation to master account
-				client.Statuses.Update(commandParameters, nil)
-				SendDirectMessage(client, master, "I have posted your tweet.")
-				// Command is just to confirm that the bot is active
-				// AYT means "Are You There?
-			case "AYT":
-				// Set parameters for a response via direct message
-				SendDirectMessage(client, master, "Hey, boss. I'm active. What's up?")
-				// RTW is confirmation or denial of retweeting request
-			case "RTW":
-				// If reply is yes, then retweet
-				if commandParameters == "YES" {
-					client.Statuses.Update(retweetCandidate.Text, nil)
-				}
-			// Request for a list of followers
-			case "FLS":
-				userParams := &twitter.FollowerListParams{
-					ScreenName: servant,
-				}
-				listOfUsers, httpResponse, err := client.Followers.List(userParams)
-				if err != nil {
-					fmt.Println(listOfUsers, httpResponse, err)
-				}
+		// Command is to tweet something
+		case "TWT":
+			// Tweets the command parameters and sends confirmation to master account
+			client.Statuses.Update(commandParameters, nil)
+			SendDirectMessage(client, master, "I have posted your tweet.")
+			// Command is just to confirm that the bot is active
+			// AYT means "Are You There?
+		case "AYT":
+			// Set parameters for a response via direct message
+			SendDirectMessage(client, master, "Hey, boss. I'm active. What's up?")
+			// RTW is confirmation or denial of retweeting request
+		case "RTW":
+			// If reply is yes, then retweet
+			if commandParameters == "YES" {
+				client.Statuses.Update(retweetCandidate.Text, nil)
+			}
+		// Request for a list of followers
+		case "FLS":
+			userParams := &twitter.FollowerListParams{
+				ScreenName: servant,
+			}
+			listOfUsers, httpResponse, err := client.Followers.List(userParams)
+			if err != nil {
+				fmt.Println(listOfUsers, httpResponse, err)
+			}
 
-				var response string = ""
+			var response string = ""
 
-				var followers []string
+			var followers []string
 
-				for _,user := range listOfUsers.Users{
-					response = response + "\n" + "@" + user.ScreenName
-					followers = append(followers, user.ScreenName)
-				}
-				// Replies with all followers
-				switch commandParameters {
-				case "ALL":
+			for _, user := range listOfUsers.Users {
+				response = response + "\n" + "@" + user.ScreenName
+				followers = append(followers, user.ScreenName)
+			}
+			// Replies with all followers
+			switch commandParameters {
+			case "ALL":
+				fmt.Println(response)
+
+				SendDirectMessage(client, master, response)
+			case "NEW":
+				// Check if followers.txt file exists
+				if _, err := os.Stat("followers.txt"); err == nil {
+					originalList := ProcessKeyFile("followers.txt")
+					delta := CompareSlices(followers, originalList)
+					response = ""
+					for _, screenName := range delta {
+						response = response + "\n@" + screenName
+					}
+				} else {
+					response := "No previous followers file. Send FLS ALL to create a new one."
 					fmt.Println(response)
-
-					SendDirectMessage(client, master, response )
-				case "NEW":
-					// Check if followers.txt file exists
-					if _, err := os.Stat("followers.txt"); err == nil {
-						originalList := ProcessKeyFile("followers.txt")
-						delta := CompareSlices(followers, originalList)
-						response = ""
-						for _, screenName := range delta {
-							response = response + "\n@" + screenName
-						}
-					} else {
-						response := "No previous followers file. Send FLS ALL to create a new one."
-						fmt.Println(response)
-					}
-					if response == "" {
-						response = "No new followers"
-					}
-					// Save list of followers
-					WriteTextFile("followers.txt", followers)
-					SendDirectMessage(client, master, response )
 				}
-			// Command is to return list of users that are being followed
-			case "LST":
-				userParams := &twitter.FriendListParams{
-					ScreenName: servant,
+				if response == "" {
+					response = "No new followers"
 				}
-				listOfUsers, httpResponse, err := client.Friends.List(userParams)
-				if err != nil {
-					fmt.Println(listOfUsers, httpResponse, err)
-				}
-
-				var response string = ""
-
-				var friends []string
-
-				for _,user := range listOfUsers.Users{
-					response = response + "\n" + "@" + user.ScreenName
-					friends = append(friends, user.ScreenName)
-				}
+				// Save list of followers
+				WriteTextFile("followers.txt", followers)
 				SendDirectMessage(client, master, response)
-			// Command is to follow a user
-			case "FLW":
-				newFriendParams := &twitter.FriendshipCreateParams{
-					ScreenName: commandParameters,
-				}
-				client.Friendships.Create(newFriendParams)
-				response := "Following " + commandParameters
-				SendDirectMessage(client, master, response)
+			}
+		// Command is to return list of users that are being followed
+		case "LST":
+			userParams := &twitter.FriendListParams{
+				ScreenName: servant,
+			}
+			listOfUsers, httpResponse, err := client.Friends.List(userParams)
+			if err != nil {
+				fmt.Println(listOfUsers, httpResponse, err)
+			}
 
-			// Command is to unfollow a user
-			case "UFL":
-				unfriendParams := &twitter.FriendshipDestroyParams{
-					ScreenName: commandParameters,
-				}
-				client.Friendships.Destroy(unfriendParams)
-				response := "I stopped following " + commandParameters
-				SendDirectMessage(client, master, response)
+			var response string = ""
+
+			var friends []string
+
+			for _, user := range listOfUsers.Users {
+				response = response + "\n" + "@" + user.ScreenName
+				friends = append(friends, user.ScreenName)
+			}
+			SendDirectMessage(client, master, response)
+		// Command is to follow a user
+		case "FLW":
+			newFriendParams := &twitter.FriendshipCreateParams{
+				ScreenName: commandParameters,
+			}
+			client.Friendships.Create(newFriendParams)
+			response := "Following " + commandParameters
+			SendDirectMessage(client, master, response)
+
+		// Command is to unfollow a user
+		case "UFL":
+			unfriendParams := &twitter.FriendshipDestroyParams{
+				ScreenName: commandParameters,
+			}
+			client.Friendships.Destroy(unfriendParams)
+			response := "I stopped following " + commandParameters
+			SendDirectMessage(client, master, response)
 		}
 
 	} else {
-		fmt.Println( "Received message WAS NOT an order")
+		fmt.Println("Received message WAS NOT an order")
 	}
 }
 
 // Sends a direct message to the master account
-func SendDirectMessage(client *twitter.Client, screenName string, messageText string){
+func SendDirectMessage(client *twitter.Client, screenName string, messageText string) {
 	dmParams := &twitter.DirectMessageNewParams{
 		ScreenName: screenName,
-		Text: messageText,
+		Text:       messageText,
 	}
 	directMessage, httpResponse, err := client.DirectMessages.New(dmParams)
 	if err != nil {
@@ -163,7 +163,7 @@ func ExamineTweet(tweetText string) bool {
 	filterRegex := ""
 
 	// Assemble combined string for regex
-	for _,word := range filterWords{
+	for _, word := range filterWords {
 		word = strings.ToUpper(word)
 		if filterRegex == "" {
 			filterRegex = "(" + word + ")+"
@@ -181,7 +181,7 @@ func ExamineTweet(tweetText string) bool {
 	interestingTweet := regexp.MustCompile(filterRegex)
 
 	// Handle tweets that are possible retweet targets
-	if interestingTweet.MatchString(tweetText){
+	if interestingTweet.MatchString(tweetText) {
 		commandArray := interestingTweet.FindStringSubmatch(tweetText)
 		command := commandArray[0]
 		fmt.Println("INFO: String match: " + command)
@@ -190,4 +190,3 @@ func ExamineTweet(tweetText string) bool {
 
 	return false
 }
-
